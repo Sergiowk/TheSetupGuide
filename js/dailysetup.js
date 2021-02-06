8f2ebc4967abc46a7bcbd0f8fa65be507b6e7c6a
Sergiowk <sergiowk94@gmail.com>
2021-02-06 18:15:28 +0100

    InstagramFeed added
    Recommended Instagram profiles added

---
+++ js/dailysetup.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    (function($){
+        $(window).on('load', function(){
+            $.instagramFeed({
+                'username': 'dailysetuptech',
+                'container': "#recommended1",
+                'display_profile': false,
+                'display_biography': false,
+                'display_gallery': true,
+                'display_captions': false,
+                'callback': null,
+                'styling': true,
+                'items': 8,
+                'items_per_row': 4,
+                'margin': 1 
+            }),
+            $.instagramFeed({
+                'username': 'tech4gaming',
+                'container': "#recommended2",
+                'display_profile': false,
+                'display_biography': false,
+                'display_gallery': true,
+                'display_captions': false,
+                'callback': null,
+                'styling': true,
+                'items': 8,
+                'items_per_row': 4,
+                'margin': 1 
+            }),
+            $.instagramFeed({
+                'username': 'thedreamsetup',
+                'container': "#recommended3",
+                'display_profile': false,
+                'display_biography': false,
+                'display_gallery': true,
+                'display_captions': false,
+                'callback': null,
+                'styling': true,
+                'items': 8,
+                'items_per_row': 4,
+                'margin': 1 
+            });
+        });
+    })(jQuery);
+
+});
