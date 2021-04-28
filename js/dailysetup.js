2ff25f267782f583398cb3c4a7886eb2bed03224
Sergiowk <sergiowk94@gmail.com>
2021-04-28 17:28:57 +0200

    Fetching Data working with the API
    -Deleted pictures (not needed anymore)
    -Fetching Data from 3 profiles
    -Showing profile fullname and profile pic from Instagram (needed apikey to work)

--- js/dailysetup.js
+++ js/dailysetup.js
@@ -1,28 +1,49 @@
 $(document).ready(function(){
-    var profile ='dailysetuptech'
-    const url = 'https://instagram28.p.rapidapi.com/user_info?user_name='+ profile;
-    const xrapidapihost = 'instagram28.p.rapidapi.com';
+   
+    
 
-
-    fetch(url, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": xrapidapikey,
-            "x-rapidapi-host": xrapidapihost
-        }
-    })
-    .then(response => response.json())
-    .then(data=> 
-        console.log(data))
-    .catch(err => {
-        console.error(err);
+    $(".fetch").click(function(){
+        console.log("FetchDataExecuted");
+        fetchData();
     });
 
     $(".logos").click(function(){
         link=$(this).attr("href");
         window.open(link,"link" );
-
+    
         //return false;
     });
-
+    
 });
+
+function fetchData(){
+
+    var profile = ['dailysetuptech', 'tech4gaming', 'thedreamsetup'] ;
+    for (var i = 0; i < 3; i++) {
+        
+        //const url = 'https://reqres.in/api/users?page=2';
+        const url = 'https://instagram28.p.rapidapi.com/user_info?user_name='+ profile[i];
+        const xrapidapihost = 'instagram28.p.rapidapi.com';
+        fetch(url, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": xrapidapikey,
+                "x-rapidapi-host": xrapidapihost
+            }
+        })
+        .then(response => response.json())
+        .then(data=> {
+            console.log(data.data.user);
+            const html = `<h1> <a href="https://www.instagram.com/${data.data.user.username}">${data.data.user.full_name}</a></h1> <p><iframe frameborder="0" style="height: 320px; overflow:scroll; width: 100%" marginheight="1" marginwidth="1" name="cboxmain" id="cboxmain" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" src="${data.data.user.profile_pic_url_hd}"/></iframe></p> `;
+            document.querySelector('#recommended')
+            .insertAdjacentHTML("afterbegin", html);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
+    
+    
+    
+
+}
