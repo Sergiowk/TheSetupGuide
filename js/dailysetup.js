2d6c37f74c02abe5004f24968be70241a7b967a2
Sergio <34040046+Sergiowk@users.noreply.github.com>
2021-04-28 17:29:55 +0200

    Using api instead instagram feed (#10)
    * Cleaning all the old configuration and starting the API Rest
    
    * Fetching Data working with the API
    
    -Deleted pictures (not needed anymore)
    -Fetching Data from 3 profiles
    -Showing profile fullname and profile pic from Instagram (needed apikey to work)

--- js/dailysetup.js
+++ js/dailysetup.js
@@ -1,58 +1,49 @@
 $(document).ready(function(){
+   
+    
 
-    (function($){
-        $(window).on('load', function(){
-            $.instagramFeed({
-                'username': 'dailysetuptech',
-                'container': "#recommended1",
-                'display_profile': false,
-                'display_biography': false,
-                'display_gallery': true,
-                'display_captions': false,
-                'callback': null,
-                'styling': true,
-                'items': 8,
-                'items_per_row': 4,
-                'margin': 1,
-                'host':'images' + ~~(Math.random() * 3333) +'-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=www.instagram.com/'
-
-            }),
-            $.instagramFeed({
-                'username': 'tech4gaming',
-                'container': "#recommended2",
-                'display_profile': false,
-                'display_biography': false,
-                'display_gallery': true,
-                'display_captions': false,
-                'callback': null,
-                'styling': true,
-                'items': 8,
-                'items_per_row': 4,
-                'margin': 1,
-                'host':'images' + ~~(Math.random() * 3333) +'-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=www.instagram.com/'
-            }),
-            $.instagramFeed({
-                'username': 'thedreamsetup',
-                'container': "#recommended3",
-                'display_profile': false,
-                'display_biography': false,
-                'display_gallery': true,
-                'display_captions': false,
-                'callback': null,
-                'styling': true,
-                'items': 8,
-                'items_per_row': 4,
-                'margin': 1,
-                'host':'images' + ~~(Math.random() * 3333) +'-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=www.instagram.com/'
-            });
-        });
-    })(jQuery);
+    $(".fetch").click(function(){
+        console.log("FetchDataExecuted");
+        fetchData();
+    });
 
     $(".logos").click(function(){
         link=$(this).attr("href");
         window.open(link,"link" );
-
+    
         //return false;
     });
-
+    
 });
+
+function fetchData(){
+
+    var profile = ['dailysetuptech', 'tech4gaming', 'thedreamsetup'] ;
+    for (var i = 0; i < 3; i++) {
+        
+        //const url = 'https://reqres.in/api/users?page=2';
+        const url = 'https://instagram28.p.rapidapi.com/user_info?user_name='+ profile[i];
+        const xrapidapihost = 'instagram28.p.rapidapi.com';
+        fetch(url, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": xrapidapikey,
+                "x-rapidapi-host": xrapidapihost
+            }
+        })
+        .then(response => response.json())
+        .then(data=> {
+            console.log(data.data.user);
+            const html = `<h1> <a href="https://www.instagram.com/${data.data.user.username}">${data.data.user.full_name}</a></h1> <p><iframe frameborder="0" style="height: 320px; overflow:scroll; width: 100%" marginheight="1" marginwidth="1" name="cboxmain" id="cboxmain" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" src="${data.data.user.profile_pic_url_hd}"/></iframe></p> `;
+            document.querySelector('#recommended')
+            .insertAdjacentHTML("afterbegin", html);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
+    
+    
+    
+
+}
